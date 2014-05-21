0a87f181991eaff7aa7697727f88a25609c4edbb
courtney mcilhenny <github@courtneymcilhenny>
2014-05-21 09:00:29 -0700

    adding underscore plugin and playing with alphs sort when a new contact is added.

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -8,9 +8,49 @@ $(document).ready(function(){
     console.log("Delete", this);
     $(this).parents(".contact").remove();
   };
+ 
+  var editContact = function(event){
+    var $contact = $(this).parents(".contact");
+    var $btn = $(this);
+    $btn.toggleClass("edit");
+    $btn.toggleClass("update");
+    $btn.html("SAVE").toggleClass("btn-attention");
+    $contact.find(".contact-item").attr("contenteditable", "true");
+
+    //Deal with contact img
+    var $contactImg = $contact.find("#contact-img");
+    var src = $contactImg.attr("src");
+    var $imgCon = $contact.find(".contact-img-con")
+    $imgCon.empty();
+    $imgCon.attr("contenteditable", "true");
+    $imgCon.html(src);
+   
+  };
+ 
+  var updateContact = function(event){
+    var $contact = $(this).parents(".contact");
+    var $btn = $(this);
+    $btn.toggleClass("edit");
+    $btn.toggleClass("update");
+    $btn.html("Edit").toggleClass("btn-attention");
+    $contact.find(".contact-item").attr("contenteditable", "false");
+    
+    var name = $contact.find(".contact-name").first().html();
+    var email = $contact.find(".contact-email").first().html();
+    var number = $contact.find(".contact-number").first().html();
+   
+    var $imgCon = $contact.find(".contact-img-con");
+    var imgUrl = $imgCon.first().html();
+    $imgCon.empty();
+    $imgCon.append( "<img id='contact-img' src='" + imgUrl + "' class='contact-img'>");
+    $imgCon.attr("contenteditable", "false");
+  };
   
   $("#contacts").on("click", ".delete", deleteContact);
  
+  $("#contacts").on("click", ".edit", editContact);
+  
+  $("#contacts").on("click", ".update", updateContact);
  
   var addContact = function(newContact){
     var contactString = ["<div id='", newContact.id, "' class='contact'>",
@@ -22,7 +62,7 @@ $(document).ready(function(){
                             "<div class='contact-item'>", newContact.number, "</div>",
           
                             "<div class='contact-actions'>",
-                            
+                              "<span class='edit btn btn-warning'>Edit</span>",
                               "<span class='delete btn btn-action'>Delete</span>",
                             "</div>",
                           "</div>"].join("");
@@ -57,17 +97,29 @@ $(document).ready(function(){
                        email: email,
                        number: number,
                        imgUrl: imgUrl };
+
     count += 1;
  
     contacts.push(newContact);
+    contacts = _.sortBy(contacts, function(contact) {
+      return contact.name;
+    });
+    $("#contacts").html('');
+
     // Call a function to add our contact to 
     //  the page.
-    addContact(newContact);
+    $.each(contacts, function(index, contact) {
+      addContact(contact);
+    });
  
   });
 
   $.get('/contacts.json').done(function(data){
     contacts = data
+    contacts = _.sortBy(contacts, function(contact) {
+      return contact.name;
+    });
+    // console.log(_.findWhere(contacts, { name: "Adam"} ));
     $.each(contacts, function(index, contact) {
       addContact(contact);
     });
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Contact.create ([
   {name: "John", email: "john@john.com", number: "917-239-2929"},
+  {name: "Adam", email: "adam@adam.com", number: "917-239-2929"},
   {name: "Sally", email: "sally@sally.com", number: "917-555-2929"},
   {name: "Sam", email: "sam@sam.com", number: "917-239-5555"}
   ])
