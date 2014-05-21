5bf2513c26d834ddadcbef1df97c4788d9ffbff5
courtney mcilhenny <github@courtneymcilhenny>
2014-05-20 19:34:29 -0700

    setting up json responce

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,16 @@
 class ContactsController < ApplicationController
 
+
   def main
+  
+  end
+
+  def index 
+    @contacts = Contact.all
+
+    respond_to do |f|
+      f.json {render :json => @contacts, :only => [:id, :name, :email, :number, :imgUrl]}
+    end
   end
   
 end
